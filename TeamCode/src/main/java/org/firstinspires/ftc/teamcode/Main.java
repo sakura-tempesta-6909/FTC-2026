a22d2d67d467d1f9dfabb740f2fec69e58242920
Yuki Terasaki <tenko0316yuki1@gmail.com>
2025-12-26 20:14:30 +0900

    コマンドベースで動いた

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
+import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
-
+import dev.nextftc.ftc.components.BulkReadComponent;
 import org.firstinspires.ftc.teamcode.intake.IntakeSubsystem;
 
 
@@ -14,13 +14,16 @@ public class Main extends NextFTCOpMode {
 
     public Main() {
         addComponents(
-                new SubsystemComponent(IntakeSubsystem.INSTANCE)
+                new SubsystemComponent(IntakeSubsystem.INSTANCE),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
         );
     }
 
     @Override
     public void onStartButtonPressed() {
         Gamepads.gamepad1().x()
-                .whenBecomesTrue(IntakeSubsystem.INSTANCE.intake);
+                .whenBecomesTrue(IntakeSubsystem.INSTANCE.intake)
+                .whenFalse(IntakeSubsystem.INSTANCE.stop);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/IntakeSubsystem.java
@@ -8,17 +8,12 @@ import dev.nextftc.hardware.powerable.SetPower;
 public class IntakeSubsystem implements Subsystem {
     public static final IntakeSubsystem INSTANCE = new IntakeSubsystem();
     private final MotorEx intakeMotor = new MotorEx("IntakeMotor");
-    public Command intake = new SetPower(intakeMotor,1.0).requires(this);
-    public Command stop = new SetPower(intakeMotor,0.0).requires(this);
+    public final Command intake = new SetPower(intakeMotor, 1.0).requires(this);
+    public final Command stop = new SetPower(intakeMotor, 0.0).requires(this);
 
     @Override
     public void initialize() {
         intakeMotor.setPower(0.0);
-    }
-
-    @Override
-    public void periodic() {
-        System.out.println("PERIODIC RUNNING");
     }
 
 }
