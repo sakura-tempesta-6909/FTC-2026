5ed4340272c833c73e3f87611b70908556653802
kai.m <kai.m@sakura-tempesta.org>
2025-12-26 17:06:30 +0900

    intake version1 by kai 12:24 19:10

--- TeamCode/src/main/java/intake/intakeSubsystem.java
+++ TeamCode/src/main/java/intake/intakeSubsystem.java
@@ -2,19 +2,27 @@ package intake;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import dev.nextftc.core.subsystems.Subsystem;
-public class intakeSubsystem {
-    private DcMotor intakeMotor;
+public class intakeSubsystem implements Subsystem {
+    private final DcMotor intakeMotor;
+    private double targetPower = 0.0;
+
 
     public intakeSubsystem(HardwareMap hardwareMap) {
-        intakeMotor = hardwareMap.get(DcMotor.class, "intakemotor");
+        intakeMotor = hardwareMap.get(DcMotor.class, "IntakeMotor");
     }
-
-    public void intakeIn() {
-        intakeMotor.setPower(1.0);
+    @Override
+    public void initialize() {
+        intakeMotor.setPower(0.0);
     }
-
+    public void setPower(double power) {
+        targetPower = power;
+    }
+    @Override
+    public void periodic() {
+        System.out.println("PERIODIC RUNNING");
+        intakeMotor.setPower(targetPower);
+    }
     public void stop() {
         intakeMotor.setPower(0.0);
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,45 +1,26 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import dev.nextftc.core.commands.Command;
 import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.core.commands.CommandManager;
 
 import intake.intakeCommand;
+import intake.intakeSubsystem;
 
 
 @TeleOp(name = "Main")
 public class Main extends NextFTCOpMode {
+    private intakeSubsystem intake;
+    private Command myCommand;
 
     @Override
     public void onInit() {
+        telemetry.addData("init", "done");
+        telemetry.update();
+        intake = new intakeSubsystem(hardwareMap);
+        myCommand = new intakeCommand(intake,1.0);
     }
 
     @Override
@@ -52,16 +33,27 @@ public class Main extends NextFTCOpMode {
 
     @Override
     public void onUpdate() {
-        if (gamepad2.xWasPressed()) {
-            new intakeCommand();
-        } else if (gamepad2.yWasPressed()) {
-            stop();
+        telemetry.addLine("UPDATE LOOP");
+        telemetry.update();
+        CommandManager.INSTANCE.run();
+        if (gamepad1.xWasPressed()) {
+            telemetry.addLine("X PRESSED");
+            telemetry.update();
+            myCommand.schedule();
+        }
+        if (gamepad1.yWasPressed()) {
+            telemetry.addLine("Y PRESSED");
+            telemetry.update();
+            myCommand.cancel();
         }
     }
 
+
     @Override
     public void onStop() {
-            stop();
+        telemetry.addData("stop", "done");
+        telemetry.update();
+        CommandManager.INSTANCE.cancelAll();
     }
 }
 
