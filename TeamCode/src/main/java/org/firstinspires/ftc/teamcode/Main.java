c2593ca15a33ff68867e6982473b99e2acfb469e
Yuki Terasaki <tenko0316yuki1@gmail.com>
2025-12-28 00:23:06 +0900

    Show running commands and elapsed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import dev.nextftc.core.commands.CommandManager;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
@@ -14,11 +15,12 @@ import org.firstinspires.ftc.teamcode.subsystem.IntakeSubsystem;
 
 import java.util.List;
 
-
 @TeleOp(name = "Main")
 public class Main extends NextFTCOpMode {
 
-    private PanelsTelemetry panelsTelemetry = PanelsTelemetry.INSTANCE;
+    private final PanelsTelemetry panelsTelemetry = PanelsTelemetry.INSTANCE;
+    private final ElapsedTime loopTimer = new ElapsedTime();
+    private int lastSnapshotSize = 0;
 
     public Main() {
         addComponents(
@@ -40,8 +42,19 @@ public class Main extends NextFTCOpMode {
 
     @Override
     public void onUpdate() {
-        List<String> commands = CommandManager.INSTANCE.snapshot();
-        panelsTelemetry.getTelemetry().addData("command", commands.get(commands.size() - 1));
+        //実行時間表示
+        double dt = loopTimer.seconds();
+        loopTimer.reset();
+
+        //実行しているコマンドを表示
+        List<String> snapshot = CommandManager.INSTANCE.snapshot();
+        int currentSize = snapshot.size();
+        int fromIndex = Math.min(lastSnapshotSize, currentSize);
+        List<String> running = snapshot.subList(fromIndex, currentSize);
+        lastSnapshotSize = currentSize;
+
+        panelsTelemetry.getTelemetry().addData("dt", dt);
+        panelsTelemetry.getTelemetry().addData("running", String.join(", ", running));
         panelsTelemetry.getTelemetry().update(telemetry);
     }
 }
