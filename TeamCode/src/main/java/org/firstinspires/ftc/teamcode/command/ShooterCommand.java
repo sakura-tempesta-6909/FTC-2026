6c2ffa4931aa7a51f7d6d0a6d19046c4fcf16ca5
kai.m <kai.m@sakura-tempesta.org>
2026-01-15 18:55:39 +0900

    shooter回りきってからfeed+pid制御

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/ShooterCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/ShooterCommand.java
@@ -1,25 +1,26 @@
 package org.firstinspires.ftc.teamcode.command;
 
 import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.commands.utility.LambdaCommand;
+
+import org.firstinspires.ftc.teamcode.config.Const;
 import org.firstinspires.ftc.teamcode.subsystem.FeederSubsystem;
 import org.firstinspires.ftc.teamcode.subsystem.ShooterSubsystem;
 
 public class ShooterCommand {
     public static Command shootArtifacts() {
         return new LambdaCommand()
-                .setStart(() -> ShooterSubsystem.INSTANCE.shoot().schedule())
+                .setStart(() -> ShooterSubsystem.INSTANCE.setTargetRPM(Const.Shooter.Velocity.TARGET_RPM))
                 .setUpdate(() -> {
                     if (ShooterSubsystem.INSTANCE.isAtVelocity()) {
-                        FeederSubsystem.INSTANCE.feed().schedule();
+                        FeederSubsystem.INSTANCE.setState(FeederSubsystem.FeederState.FEED);
                     } else {
-                        FeederSubsystem.INSTANCE.stop().schedule();
+                        FeederSubsystem.INSTANCE.setState(FeederSubsystem.FeederState.STOP);
                     }
                 })
-                        .setStop(i -> FeederSubsystem.INSTANCE.stop().schedule())
+                .setStop(i -> FeederSubsystem.INSTANCE.setState(FeederSubsystem.FeederState.STOP))
                         .setIsDone(() -> false)
-                        .requires(FeederSubsystem.INSTANCE)
+                        .requires(ShooterSubsystem.INSTANCE,FeederSubsystem.INSTANCE)
                 .named("shootArtifacts");
     }
 
@@ -28,11 +29,10 @@ public class ShooterCommand {
 
     public static Command reverseArtifacts() {
         return new LambdaCommand()
-                .setStart(() -> new ParallelGroup(
-                                ShooterSubsystem.INSTANCE.reverse(),
-                                FeederSubsystem.INSTANCE.retract()
-                        ).schedule()
-                )
+                .setStart(() -> {
+                    ShooterSubsystem.INSTANCE.setReverseTargetRPM(Const.Shooter.Velocity.REVERSE_TARGET_RPM);
+                    FeederSubsystem.INSTANCE.setState(FeederSubsystem.FeederState.RETRACT);
+                })
                 .setIsDone(() -> false)
                 .requires(ShooterSubsystem.INSTANCE, FeederSubsystem.INSTANCE)
                 .named("reverseArtifacts");
@@ -40,11 +40,10 @@ public class ShooterCommand {
 
     public static Command stopAll() {
         return new LambdaCommand()
-                .setStart(() -> new ParallelGroup(
-                                ShooterSubsystem.INSTANCE.stop(),
-                                FeederSubsystem.INSTANCE.stop()
-                        ).schedule()
-                )
+                .setStart(() -> {
+                    ShooterSubsystem.INSTANCE.stop().schedule();
+                    FeederSubsystem.INSTANCE.setState(FeederSubsystem.FeederState.STOP);
+                })
                 .setIsDone(() -> true)
                 .named("stopAll");
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/FeederSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/FeederSubsystem.java
@@ -7,13 +7,34 @@ import dev.nextftc.hardware.powerable.SetPower;
 import org.firstinspires.ftc.teamcode.config.Const;
 
 public class FeederSubsystem implements Subsystem {
+    public enum FeederState {
+        FEED,
+        STOP,
+        RETRACT
+    }
+
     public static final FeederSubsystem INSTANCE = new FeederSubsystem();
     private MotorEx feederMotor;
+    private FeederState state = FeederState.STOP;
+
+    public void setState(FeederState state) {
+        this.state = state;
+    }
+
 
     @Override
     public void initialize() {
         feederMotor = new MotorEx(Const.Feeder.Motor.NAME);
         feederMotor.reverse();
+    }
+
+    @Override
+    public void periodic() {
+        switch (state) {
+            case FEED -> feederMotor.setPower(Const.Feeder.Power.FEED);
+            case RETRACT -> feederMotor.setPower(Const.Feeder.Power.RETRACT);
+            case STOP -> feederMotor.setPower(0.0);
+        }
     }
 
     public Command feed() {
