5a38d57db352fd90f6ef4e9f6103da78105460d5
Yuki Terasaki <tenko0316yuki1@gmail.com>
2026-01-30 01:48:01 +0900

    Auto Finish

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ShooterSubsystem.java
@@ -2,22 +2,23 @@ package org.firstinspires.ftc.teamcode.subsystem;
 
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 import dev.nextftc.control.ControlSystem;
 import dev.nextftc.control.KineticState;
 import dev.nextftc.control.feedback.PIDCoefficients;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.impl.MotorEx;
-import org.firstinspires.ftc.teamcode.config.Const;
-import org.firstinspires.ftc.teamcode.config.PIDTuning;
-import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.config.Const;
+import org.firstinspires.ftc.teamcode.config.PIDTuning;
+
 
 @Configurable
 public class ShooterSubsystem implements Subsystem {
@@ -36,23 +37,7 @@ public class ShooterSubsystem implements Subsystem {
     private double a;
     private double b;
     private int lastSnapshotSize = 0;
-    private HardwareMap hardwareMap;
     private Telemetry telemetry;
-
-
-    public double getDistanceToTag(double Ta) {
-        double scale = 196.1;
-        double b = Math.pow(Ta, -0.8030557);
-        double distanceToTag = scale * b;
-        return distanceToTag;
-    }
-
-    public double getDistance(double distanceToTag) {
-        a = Math.pow(distanceToTag, 2);
-        b = a - 4225;
-        distance = Math.pow(b, 0.5);
-        return distance;
-    }
 
     @Override
     public void initialize() {
@@ -64,9 +49,9 @@ public class ShooterSubsystem implements Subsystem {
         targetVelocity = 0.0;
         controller.setGoal(new KineticState(0.0, 0.0));
         telemetry = panelsTelemetry.getFtcTelemetry();
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight = ActiveOpMode.hardwareMap().get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
-        imu = hardwareMap.get(IMU.class, "imu");
+        imu = ActiveOpMode.hardwareMap().get(IMU.class, "imu");
         RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
         imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
         limelight.start();
@@ -91,17 +76,18 @@ public class ShooterSubsystem implements Subsystem {
             telemetry.addData("Ty", llResult.getTy());
             telemetry.addData("Ta", llResult.getTa());
         }
-        panelsTelemetry.getTelemetry().update();
         PanelsTelemetry.INSTANCE.getTelemetry().addData("P", pidCoefficients.kP);
         PanelsTelemetry.INSTANCE.getTelemetry().addData("goal", controller.getGoal().getVelocity());
         PanelsTelemetry.INSTANCE.getTelemetry().addData("current", shooterMotor.getVelocity());
         PanelsTelemetry.INSTANCE.getTelemetry().addData("isAtVelocity", isAtVelocity());
+        panelsTelemetry.getTelemetry().update();
     }
 
     public void setTargetVelocity(double velocity) {
         this.targetVelocity = velocity;
         controller.setGoal(new KineticState(0.0, velocity));
     }
+
     public void setTargetRPM() {
         if (distance < 50) {
             setTargetVelocity(Const.Shooter.Velocity.LOWEST_RPM);
@@ -113,6 +99,7 @@ public class ShooterSubsystem implements Subsystem {
             setTargetVelocity(Const.Shooter.Velocity.HIGHEST_RPM);
         }
     }
+
     public void setRPMFromDistance(double distance) {
         double rpm = Const.Shooter.rpmFromDistance(distance);
         setTargetVelocity(rpm);
@@ -121,16 +108,18 @@ public class ShooterSubsystem implements Subsystem {
     public void setHighestRPM() {
         setTargetVelocity(Const.Shooter.Velocity.HIGHEST_RPM);
     }
+
     public void setMediumHighRPM() {
         setTargetVelocity(Const.Shooter.Velocity.MEDIUM_HIGH_RPM);
     }
+
     public void setNormalRPM() {
         setTargetVelocity(Const.Shooter.Velocity.NORMAL_RPM);
     }
+
     public void setLowestRPM() {
         setTargetVelocity(Const.Shooter.Velocity.LOWEST_RPM);
     }
-
 
 
     public void setReverseTargetRPM() {
@@ -144,5 +133,19 @@ public class ShooterSubsystem implements Subsystem {
 
     public boolean isAtVelocity() {
         return Math.abs(shooterMotor.getVelocity() - controller.getGoal().getVelocity()) <= Const.Shooter.Velocity.TOLERANCE;
+    }
+
+    public double getDistanceToTag(double Ta) {
+        double scale = 196.1;
+        double b = Math.pow(Ta, -0.8030557);
+        double distanceToTag = scale * b;
+        return distanceToTag;
+    }
+
+    public double getDistance(double distanceToTag) {
+        a = Math.pow(distanceToTag, 2);
+        b = a - 4225;
+        distance = Math.pow(b, 0.5);
+        return distance;
     }
 }
